8ea1200b05873503007d60f73f35fd225c66c206
Jonayah Jackson <jonayah@thinkhalcyon.com>
2023-01-05 19:59:44 -0500

    initial commit

---
+++ src/common/elements/head/HeadTitle.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+const HeadTitle = ({pageTitle}) => {
+    return ( 
+        <Head>
+            <meta charSet="utf-8" />
+            <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+            <title>{`${pageTitle} || The Fright Queen Blog`}</title>
+            <meta name="robots" content="noindex, follow" />
+            <meta name="description" content="The Official Website for The Fright Queen Blog" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+            <link rel="icon" type="image/x-icon" href={`${process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASEPATH ?? '' : ''}/favicon.ico`} />
+        </Head>
+     );
+}
+ 
+export default HeadTitle;
