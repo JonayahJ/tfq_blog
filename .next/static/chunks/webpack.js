9c8bf1c69d0976a7a65ffeb079aab55ab956ab36
Jonayah Jackson <jonayah@thinkhalcyon.com>
2023-01-21 13:18:46 -0500

    fixed ds_store server error nonsense

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f18624d0aab511ac"; }
+/******/ 		__webpack_require__.h = function() { return "49c6752c45b3b0d9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.c2ac8424431f0585.hot-update.js
+++ .next/static/webpack/webpack.c2ac8424431f0585.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c7cddd93ef159ce8"; }
+/******/ 	__webpack_require__.h = function() { return "49c6752c45b3b0d9"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
